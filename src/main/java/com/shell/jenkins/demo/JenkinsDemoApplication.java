3b16757d4ec9c6960f45cbfdab84bf07eee1a12b
sfw134 <comeback4816@163.com>
2021-11-05 11:51:25 +0800

    初始代码提交

---
+++ src/main/java/com/shell/jenkins/demo/JenkinsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.shell.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author SFW
+ * @Description TODO
+ * @Date 2021/11/5 11:41
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/shell/jenkins/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.shell.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author SFW
+ * @Description TODO
+ * @Date 2021/11/5 11:41
+ * @Version 1.0
+ **/
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "hello";
+    }
+}
